ba80b3fe7da93ca3d8da949ec8cc54780fc5134c
DIYATECH\rizwan_farooq <rizwan_farooq@diya-tech.com>
2026-02-06 17:17:34 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Alachisoft.NCache.Client;
+
+Console.WriteLine("Hello, World!");
+
+string cacheName = "demoCache";
+
+// Connect to cache
+ICache cache = CacheManager.GetCache(cacheName);
+
+Console.WriteLine("cache connected");
